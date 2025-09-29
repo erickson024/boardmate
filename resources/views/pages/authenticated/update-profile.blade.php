8b1e66b00305a914f710947397cddf03ae874ba1
boardmate <ericksonhervera@gmail.com>
2025-09-29 22:35:17 +0800

    update-profile modified

--- resources/views/pages/authenticated/update-profile.blade.php
+++ resources/views/pages/authenticated/update-profile.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 @section('title', 'Boardmate Profile Update')
 @section('content')
-@livewire('auth.update-profile')
+@livewire('profile-update.update-profile')
 @endsection
